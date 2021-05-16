956e83a6b8a641df8d4b53045ff6f7bb678ae9aa
Jane Shi <69834620+jane-cz@users.noreply.github.com>
2021-05-16 20:26:23 +0800

    Added background image

--- web-component/implement.py
+++ web-component/implement.py
@@ -1,4 +1,3 @@
-
 import option1
 import option2
 import leaderboard
@@ -9,9 +8,22 @@ PAGES = {
     "Draw Right Now": option2,
     "Competition Leaderboard": leaderboard
 }
+
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("https://www.outdoorpainter.com/wp-content/uploads/2019/09/plein-air-oil-painting-Debra-Latham-Hill-Country-Sunset.jpg")
+    }
+   .sidebar-container {
+        background: url("https://www.outdoorpainter.com/wp-content/uploads/2019/09/plein-air-oil-painting-Debra-Latham-Hill-Country-Sunset.jpg")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.sidebar.title('Navigation')
 selection = st.sidebar.selectbox("Select Options", list(PAGES.keys()))
 page = PAGES[selection]
 page.exe()
-
-
